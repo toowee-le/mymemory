3a23f7f8a4ec33d85a84dfd582e9868ee5818e2f
Tuyet Le <toowee.le@gmail.com>
2020-03-16 12:04:53 +0000

    Add animation to the burger icon on click

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelector('.nav-links li')
 
     // Toggle the burger menu
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+
+        // Animate the burger icon
+        burger.classList.toggle('toggle');
     })
 }
 
