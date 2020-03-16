4e30ea7f3c24bae8036ad26af2e16867938a3a61
Tuyet Le <toowee.le@gmail.com>
2020-03-16 11:56:12 +0000

    Add comment

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
 
+    // Toggle the burger menu
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
     })
